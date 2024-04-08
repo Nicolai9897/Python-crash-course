b0700ff281df041e6ed165466177b0875c6d5518
nicolaao <nicolaao@stud.ntnu.no>
2024-04-08 09:03:56 +0200

    Added  excersise 15-6 to 15-9.

---
+++ PCC_DV/CH_15/15-10 practising with both lilbraries/die_visuals.py
@@ -0,0 +1,24 @@
+import plotly.express as px
+import pandas
+import matplotlib
+
+from die import Die
+
+# Create two D6 dice.
+die_1 = Die()
+die_2 = Die(10)
+
+# Make some rolls, and store results in a list.
+results = [die_1.roll() + die_2.roll() for result in range(10000)]
+
+# Analyze the results.
+max_result = die_1.num_sides + die_2.num_sides
+poss_results = range(2, max_result + 1)
+frequencies = [results.count(x) for x in poss_results]
+
+
+# Visualize the results.
+title = "Results of Rolling a D6 Dice and a D10  50,000 Times"
+labels = {'x': 'Result', 'y': 'Frequency of Result'}
+fig = px.bar(x=poss_results, y=frequencies, title=title, labels=labels)
+fig.write_html('dice_visual_d6d10.html')
