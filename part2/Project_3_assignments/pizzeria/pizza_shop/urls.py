8f96e01f8906d7b67943edcac3046290d4ef3cd7
nicolaao <nicolaao@stud.ntnu.no>
2025-04-02 21:44:56 +0200

    Completed last excersises in chapter 18

--- part2/Project_3_assignments/pizzeria/pizza_shop/urls.py
+++ part2/Project_3_assignments/pizzeria/pizza_shop/urls.py
@@ -8,6 +8,11 @@ app_name = 'pizza_shop'
 urlpatterns = [
     # Home page 
     path('', views.index, name='index'),
+    # Page that shows all the pizzas
+    path('pizzas/', views.pizzas, name='pizzas'),
+    # Detail page for a single pizza
+    path('pizzas/<int:pizza_id>/', views.pizza, name='pizza'),
+    
 ]
 
 
--- part2/Project_3_assignments/pizzeria/pizza_shop/views.py
+++ part2/Project_3_assignments/pizzeria/pizza_shop/views.py
@@ -1,8 +1,20 @@
 from django.shortcuts import render
 
+from .models import Pizza
 
 def index(request):
     """The home page for the Pizza shop"""
     return render(request, 'pizza_shop/index.html')
 
+def pizzas(request):
+    """Show all pizzas"""
+    pizzas = Pizza.objects.order_by('name')
+    context = {'pizzas': pizzas}
+    return render(request, 'pizza_shop/pizzas.html', context)
 
+def pizza(request, pizza_id):
+    """Show a single pizza and all its topppings"""
+    pizza = Pizza.objects.get(id=pizza_id)
+    toppings = pizza.topping_set.order_by('topping')
+    context = {'pizza': pizza, 'toppings': toppings}
+    return render(request, 'pizza_shop/pizza.html', context)
